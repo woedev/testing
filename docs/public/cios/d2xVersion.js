3125bf1aad2660e82ce11f37b2ffde17e09ab7eb
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 18:33:21 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -8,26 +8,26 @@ $(document).ready(function () {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', lastKnownVersion + '-vWii');
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', lastKnownVersion);
     })
-});
 
+    const font = new FontFace("DOSVGA", "url(https://woedev.github.io/testing/cios/LessPerfectDOSVGA.woff)", {
+        style: "normal",
+        weight: "normal",
+    });
+    
+    font.load().then(function (font) {
+        document.fonts.add(font);
+    
+        const canvas = document.getElementById("d2x-canvas");
+        const ctx = canvas.getContext("2d");
+        const image = document.getElementById("d2x-image");
+    
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
+        ctx.drawImage(image, 0, 0);
+    
+        ctx.font = "16px DOSVGA";
+        ctx.fillStyle = "#D3D3D3";
+        ctx.fillText("<d2x-v11-beta3>", 120, 133);
+    });
 
-const font = new FontFace("DOSVGA", "url(https://woedev.github.io/testing/cios/LessPerfectDOSVGA.woff)", {
-    style: "normal",
-    weight: "normal",
-});
-
-font.load().then(function (font) {
-    document.fonts.add(font);
-
-    const canvas = document.getElementById("d2x-canvas");
-    const ctx = canvas.getContext("2d");
-    const image = document.getElementById("d2x-image");
-
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    ctx.drawImage(image, 0, 0);
-
-    ctx.font = "16px DOSVGA";
-    ctx.fillStyle = "#D3D3D3";
-    ctx.fillText("<d2x-v11-beta3>", 120, 133);
 });
