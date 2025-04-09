6045e2862f183c7ab0a08c3bb008264e8052e23f
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 19:20:25 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -1,5 +1,8 @@
 const lastKnownVersion = "d2x-v11-beta3";
 
+const canvas = document.getElementById("d2x-canvas");
+const ctx = canvas.getContext("2d");
+
 $(document).ready(function () {
     $.getJSON("https://api.github.com/repos/wiidev/d2x-cios/releases/latest").done(function (json) {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', json.name + '-vWii');
@@ -9,15 +12,24 @@ $(document).ready(function () {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', lastKnownVersion);
     })
 
-    const canvas = document.getElementById("d2x-canvas");
-    const ctx = canvas.getContext("2d");
-    const image = document.getElementById("d2x-image");
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
 
-    ctx.drawImage(image, 0, 0);
+
+    //const image = document.getElementById("d2x-image");
+
+    const image = new Image(640, 480);
+    image.onload = drawImageActualSize;
+    image.src = "https://raw.githubusercontent.com/woedev/testing/refs/heads/master/docs/public/images/cios/d2x_v11_248.png";
+});
+
+
+function drawImageActualSize() {
+    // Use the intrinsic size of image in CSS pixels for the canvas element
+    canvas.width = this.naturalWidth;
+    canvas.height = this.naturalHeight;
+  
+    ctx.drawImage(this, 0, 0);
 
     ctx.font = "16px Arial";
     ctx.fillStyle = "#D3D3D3";
     ctx.fillText("<d2x-v11-beta3>", 120, 133);
-});
+  }
