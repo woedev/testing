95abce96a10f821515852964e0c70e8235fb7992
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 20:27:33 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -12,23 +12,15 @@ $(document).ready(function () {
 });
 */
 
-const font = new FontFace("DOSVGA", "url(https://woedev.github.io/testing/cios/LessPerfectDOSVGA.woff)", {
-    style: "normal",
-    weight: "normal",
-});
 
-font.load().then(function (font) {
-    document.fonts.add(font);
+const canvas = document.getElementById("d2x-canvas");
+const ctx = canvas.getContext("2d");
+const image = document.getElementById("d2x-image");
 
-    const canvas = document.getElementById("d2x-canvas");
-    const ctx = canvas.getContext("2d");
-    const image = document.getElementById("d2x-image");
+canvas.width = image.naturalWidth;
+canvas.height = image.naturalHeight;
+ctx.drawImage(image, 0, 0);
 
-    canvas.width = image.naturalWidth;
-    canvas.height = image.naturalHeight;
-    ctx.drawImage(image, 0, 0);
-
-    ctx.font = "16px console";
-    ctx.fillStyle = "#D3D3D3";
-    ctx.fillText("<d2x-v11-beta3>", 120, 133);
-});
+ctx.font = "16px console";
+ctx.fillStyle = "#D3D3D3";
+ctx.fillText("<d2x-v11-beta3>", 120, 133);
