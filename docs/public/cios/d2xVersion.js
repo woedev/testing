33bc7b5d77a404815c6f0e1da1ec47aef162c1da
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 18:02:14 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -1,7 +1,7 @@
 const lastKnownVersion = "d2x-v11-beta3";
 
 $(document).ready(function () {
-    $.getJSON("https://apix.github.com/repos/wiidev/d2x-cios/releases/latest").done(function (json) {
+    $.getJSON("https://api.github.com/repos/wiidev/d2x-cios/releases/latest").done(function (json) {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', json.name + '-vWii');
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', json.name);
     }).fail(function () {
@@ -11,23 +11,23 @@ $(document).ready(function () {
 });
 
 
-const font = new FontFace("DOSVGA", "url(./public/cios/LessPerfectDOSVGA.woff)", {
+const font = new FontFace("DOSVGA", "url(/cios/LessPerfectDOSVGA.woff)", {
     style: "normal",
     weight: "normal",
-  });
+});
 
-font.load().then(function(font){
-  document.fonts.add(font);
+font.load().then(function (font) {
+    document.fonts.add(font);
 
-  const canvas = document.getElementById("d2x-canvas");
-  const ctx = canvas.getContext("2d");
-  const image = document.getElementById("d2x-image");
+    const canvas = document.getElementById("d2x-canvas");
+    const ctx = canvas.getContext("2d");
+    const image = document.getElementById("d2x-image");
 
-  canvas.width = image.naturalWidth;
-  canvas.height = image.naturalHeight;
-  ctx.drawImage(image, 0, 0);
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+    ctx.drawImage(image, 0, 0);
 
-  ctx.font = "16px DOSVGA";
-  ctx.fillStyle = "#D3D3D3";
-  ctx.fillText("<d2x-v11-beta3>", 120, 133);
+    ctx.font = "16px DOSVGA";
+    ctx.fillStyle = "#D3D3D3";
+    ctx.fillText("<d2x-v11-beta3>", 120, 133);
 });
