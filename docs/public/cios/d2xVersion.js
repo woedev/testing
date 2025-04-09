5798c19ab47b89888672646f1400f900ab39032b
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 20:47:29 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -21,6 +21,6 @@ canvas.width = image.naturalWidth;
 canvas.height = image.naturalHeight;
 ctx.drawImage(image, 0, 0);
 
-ctx.font = "16px console";
+ctx.font = "16px DOSVGA";
 ctx.fillStyle = "#D3D3D3";
 ctx.fillText("<d2x-v11-beta3>", 120, 133);
