685c955b7e5d5927a2d30a172593c6426a2ad169
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 19:29:16 +0000

    Testing

--- docs/cios.md
+++ docs/cios.md
@@ -355,3 +355,17 @@ Do not attempt to install a Wii/vWii IOS or System Menu on the Wii mini. Doing s
 ::::
 
 :::::
+
+<script>
+const canvas = document.getElementById("d2x-canvas");
+const ctx = canvas.getContext("2d");
+const image = document.getElementById("d2x-image");
+    
+canvas.width = image.naturalWidth;
+canvas.height = image.naturalHeight;
+ctx.drawImage(image, 0, 0);
+
+ctx.font = "16px DOSVGA";
+ctx.fillStyle = "#D3D3D3";
+ctx.fillText("<d2x-v11-beta3>", 120, 133);
+</script>
--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -1,8 +1,5 @@
 const lastKnownVersion = "d2x-v11-beta3";
 
-var canvas = document.getElementById("d2x-canvas");
-var ctx = canvas.getContext("2d");
-
 $(document).ready(function () {
     $.getJSON("https://api.github.com/repos/wiidev/d2x-cios/releases/latest").done(function (json) {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', json.name + '-vWii');
@@ -11,25 +8,4 @@ $(document).ready(function () {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', lastKnownVersion + '-vWii');
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', lastKnownVersion);
     })
-
-
-
-    //const image = document.getElementById("d2x-image");
-
-    const image = new Image(640, 480);
-    image.onload = drawImageActualSize;
-    image.src = "https://raw.githubusercontent.com/woedev/testing/refs/heads/master/docs/public/images/cios/d2x_v11_248.png";
 });
-
-
-function drawImageActualSize() {
-    // Use the intrinsic size of image in CSS pixels for the canvas element
-    canvas.width = this.naturalWidth;
-    canvas.height = this.naturalHeight;
-  
-    ctx.drawImage(this, 0, 0);
-
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#D3D3D3";
-    ctx.fillText("<d2x-v11-beta3>", 120, 133);
-  }
