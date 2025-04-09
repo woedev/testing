b2807d77b7252ff4f5b189419d5949eef1554484
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 18:25:13 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 });
 
 
-const font = new FontFace("DOSVGA", 'url(./LessPerfectDOSVGA.woff) format("woff")', {
+const font = new FontFace("DOSVGA", 'url(https://woedev.github.io/testing/cios/LessPerfectDOSVGA.woff) format("woff")', {
     style: "normal",
     weight: "normal",
 });
