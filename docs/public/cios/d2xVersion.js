77cbb7e800ab5e08a57f6606e8d179ebbc164c8d
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 18:48:45 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 document.getElementById("d2x-image").addEventListener("load", (e) => {
     const canvas = document.getElementById("d2x-canvas");
     const ctx = canvas.getContext("2d");
+    const image = document.getElementById("d2x-image");
     canvas.width = image.naturalWidth;
     canvas.height = image.naturalHeight;
 
