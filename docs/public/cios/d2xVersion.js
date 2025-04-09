49f31c851b5e6d5f5ae661debd80f3184162ea15
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 23:18:54 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -14,24 +14,13 @@ $(document).ready(function () {
 (function () {
     const canvas = document.getElementById("d2x-canvas");
     const ctx = canvas.getContext("2d");
-    //const image = document.getElementById("d2x-image");
 
     let image = new Image();
-
     image.onload = () => {
         ctx.drawImage(image, 0, 0);
         ctx.font = "16px DOSVGA";
         ctx.fillStyle = "#D3D3D3";
         ctx.fillText("<" + "d2x-v11-beta3" + ">", 120, 133);
     };
-    
     image.src = './images/cios/d2x_v11_248.png';
-
-//    canvas.width = image.naturalWidth;
-//    canvas.height = image.naturalHeight;
-//    ctx.drawImage(image, 0, 0);
-
-
-
-    //document.getElementById("d2x-image").src = canvas.toDataURL();
 })();
