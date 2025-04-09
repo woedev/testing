36ebeffd12d6da46d05ced9e2224a90c6bf5b834
woedev <121635463+woedev@users.noreply.github.com>
2025-04-09 23:04:06 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
         ctx.drawImage(image, 0, 0);
     };
     
-    img.src = './public/images/cios/d2x_v11_248.png';
+    image.src = './public/images/cios/d2x_v11_248.png';
 
 //    canvas.width = image.naturalWidth;
 //    canvas.height = image.naturalHeight;
