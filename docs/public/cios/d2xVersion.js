ce7a020a8b5a80831f16b576ccb4a6355a15e6f0
woedev <121635463+woedev@users.noreply.github.com>
2025-04-10 17:28:24 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -45,9 +45,9 @@ function loadImage(imageID) {
         ctx.font = "16px DOSVGA";
         ctx.fillStyle = "#D3D3D3";
         ctx.fillText("<" + "d2x-v11-beta3" + ">", 120, 133);
+        sharpen(ctx, 640, 480, 0.1);
     };
     image.src = 'images/cios/' + canvas.id + '.png';
-    console.log(canvas.toDataURL('image/jpeg', 1.0));
 }
 
 
@@ -73,3 +73,53 @@ function loadImage(imageID) {
     image.src = 'images/cios/' + canvas.id + '.png';
 }
 */
+
+function sharpen(ctx, w, h, mix) {
+
+    var weights = [0, -1, 0, -1, 5, -1, 0, -1, 0],
+        katet = Math.round(Math.sqrt(weights.length)),
+        half = (katet * 0.5) | 0,
+        dstData = ctx.createImageData(w, h),
+        dstBuff = dstData.data,
+        srcBuff = ctx.getImageData(0, 0, w, h).data,
+        y = h;
+
+    while (y--) {
+
+        x = w;
+
+        while (x--) {
+
+            var sy = y,
+                sx = x,
+                dstOff = (y * w + x) * 4,
+                r = 0, g = 0, b = 0, a = 0;
+
+            for (var cy = 0; cy < katet; cy++) {
+                for (var cx = 0; cx < katet; cx++) {
+
+                    var scy = sy + cy - half;
+                    var scx = sx + cx - half;
+
+                    if (scy >= 0 && scy < h && scx >= 0 && scx < w) {
+
+                        var srcOff = (scy * w + scx) * 4;
+                        var wt = weights[cy * katet + cx];
+
+                        r += srcBuff[srcOff] * wt;
+                        g += srcBuff[srcOff + 1] * wt;
+                        b += srcBuff[srcOff + 2] * wt;
+                        a += srcBuff[srcOff + 3] * wt;
+                    }
+                }
+            }
+
+            dstBuff[dstOff] = r * mix + srcBuff[dstOff] * (1 - mix);
+            dstBuff[dstOff + 1] = g * mix + srcBuff[dstOff + 1] * (1 - mix);
+            dstBuff[dstOff + 2] = b * mix + srcBuff[dstOff + 2] * (1 - mix)
+            dstBuff[dstOff + 3] = srcBuff[dstOff + 3];
+        }
+    }
+
+    ctx.putImageData(dstData, 0, 0);
+}
