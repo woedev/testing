074cca27cbcf5f52f86bb58a7af3b09605ebca93
woedev <121635463+woedev@users.noreply.github.com>
2025-04-10 02:00:14 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     loadImage("d2x_v11_251");
 })();
 
-
+/*
 function loadImage(imageID) {
     const canvas = document.getElementById(imageID);
     const ctx = canvas.getContext("2d");
@@ -32,6 +32,32 @@ function loadImage(imageID) {
         ctx.fillStyle = "#D3D3D3";
         ctx.imageSmoothingEnabled = false;
         ctx.textRendering = "optimizeLegibility";
+        ctx.fontKerning = "none";
+        ctx.fillText("<" + "d2x-v11-beta3" + ">", 120, 133);
+    };
+    image.src = 'images/cios/' + canvas.id + '.png';
+}
+*/
+
+function loadImage(imageID) {
+    const ratio = window.devicePixelRatio;
+    const canvas = document.getElementById(imageID);
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = 640 * ratio;
+    canvas.height = 480 * ratio;
+    canvas.style.width = "640px";
+    canvas.style.height = "480px";
+    ctx.scale(ratio, ratio);
+
+    let image = new Image();
+    image.onload = () => {
+        ctx.drawImage(image, 0, 0);
+        ctx.font = "16px DOSVGA";
+        ctx.fillStyle = "#D3D3D3";
+        ctx.imageSmoothingEnabled = false;
+        ctx.textRendering = "optimizeLegibility";
+        ctx.fontKerning = "none";
         ctx.fillText("<" + "d2x-v11-beta3" + ">", 120, 133);
     };
     image.src = 'images/cios/' + canvas.id + '.png';
