8918858c553d37b6a6a6813256c23c634bbb3851
woedev <121635463+woedev@users.noreply.github.com>
2025-04-11 04:14:02 +0000

    Testing

--- docs/public/cios/d2xVersion.js
+++ docs/public/cios/d2xVersion.js
@@ -1,16 +1,10 @@
 
-/*
 $(document).ready(function () {
     $.getJSON("https://api.github.com/repos/wiidev/d2x-cios/releases/latest").done(function (json) {
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', json.name + '-vWii');
         document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', json.name);
-    }).fail(function () {
-        const lastKnownVersion = "d2x-v11-beta3";
-        document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion-vWii', lastKnownVersion + '-vWii');
-        document.body.innerHTML = document.body.innerHTML.replaceAll('d2x-currentversion', lastKnownVersion);
-    })
 });
-*/
+
 
 /*
 (async function () {
